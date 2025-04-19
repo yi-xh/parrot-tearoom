f519e7bc069b28325267b6495fb90fcbed23f1e7
yi-xh <yixh5359@gmail.com>
2025-04-19 23:28:56 +0800

    首頁排版變動+Swiper套件

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".birdSwiper", {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+    },
+});
